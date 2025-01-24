5852335b3d2d6404194d6ef1682d88580a45ef73
DHRHermans <Stijn.hermans@descheepvaartschool.be>
2025-01-24 07:32:40 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,25 @@
 
+// Zoek het canvas-element en de context op
+const canvas = document.getElementById("raceCanvas");
+const ctx = canvas.getContext("2d");
+
+// Canvasbreedte en -hoogte
+const canvasWidth = canvas.width;
+const canvasHeight = canvas.height;
+
+// Functie om het speelveld te tekenen
+function drawField() {
+    // Lichtblauwe achtergrond
+    ctx.fillStyle = "#add8e6";
+    ctx.fillRect(0, 0, canvasWidth, canvasHeight);
+
+    // Startlijn
+    ctx.fillStyle = "#000"; // Zwarte kleur
+    ctx.fillRect(50, 0, 5, canvasHeight);
+
+    // Finishlijn
+    ctx.fillRect(canvasWidth - 55, 0, 5, canvasHeight);
+}
+
+// Teken het speelveld bij het laden
+drawField();
